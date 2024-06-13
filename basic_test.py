fa6b7a6f9267f1f65ffcf32f5ad43b1851102a3c
José A. Galindo <jagalindo@us.es>
2024-06-13 12:12:07 +0200

    level 1 done

--- basic_test.py
+++ basic_test.py
@@ -2,10 +2,22 @@ from lark import Lark
 from lark.indenter import Indenter
 
 tree_grammar = r"""
-    ?start: _NL* tree
+    ?start: _NL* featuremodel
 
-    tree: NAME _NL [_INDENT tree+ _DEDENT]
+    featuremodel: "features" _NL [_INDENT feature+ _DEDENT]
+    feature: NAME _NL (_INDENT group+ _DEDENT)?
 
+    group: "or" groupspec          -> or_group
+    | "alternative" groupspec -> alternative_group
+    | "optional" groupspec    -> optional_group
+    | "mandatory" groupspec   -> mandatory_group
+    | cardinality groupspec   -> cardinality_group
+
+    groupspec: _NL _INDENT feature+ _DEDENT
+
+    cardinality: "[" INT (".." (INT | "*"))? "]"
+
+    %import common.INT
     %import common.CNAME -> NAME
     %import common.WS_INLINE
     %declare _INDENT _DEDENT
@@ -15,7 +27,7 @@ tree_grammar = r"""
 """
 
 class TreeIndenter(Indenter):
-    NL_type = '_NEWLINE'
+    NL_type = '_NL'
     OPEN_PAREN_types = []
     CLOSE_PAREN_types = []
     INDENT_type = '_INDENT'
@@ -25,13 +37,16 @@ class TreeIndenter(Indenter):
 parser = Lark(tree_grammar, parser='lalr', postlex=TreeIndenter())
 
 test_tree = """
-a
+features
     b
+        or
+            a
+            c
     c
-        d
-        e
-    f
-        g
+        alternative
+            test
+        [1..2]
+            pepe
 """
 
 def test():
--- uvl_test.py
+++ uvl_test.py
@@ -2,16 +2,16 @@ from lark import Lark
 from lark.indenter import Indenter
 
 # Custom Indenter class to handle indentation
-class MyIndenter(Indenter):
-    NL_type = 'NEWLINE'
+class TreeIndenter(Indenter):
+    NL_type = '_NL'
     OPEN_PAREN_types = []
     CLOSE_PAREN_types = []
     INDENT_type = '_INDENT'
     DEDENT_type = '_DEDENT'
-    tab_len = 1  # Set to 1 to handle tab-based indentation
+    tab_len = 8
 
 # Load the grammar from the file
-with open('grammar/uvl_level1.lark', 'r') as grammar_file:
+with open('grammar/scratch.lark', 'r') as grammar_file:
     grammar = grammar_file.read()
 
 # Load the input text from the file
@@ -19,7 +19,7 @@ with open('example/level1_model.uvl', 'r') as input_file:
     input_text = input_file.read()
 
 # Create the parser with Earley for detailed error reporting
-parser = Lark(grammar, parser='earley', postlex=MyIndenter(), debug=True)
+parser = Lark(grammar, parser='lalr', postlex=TreeIndenter(), debug=True)
 
 # Parse the input
 try:
